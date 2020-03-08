30d6c432ab4bd743074bb17989b39462f6ab5b54
Gikoskos <georgekoskerid@outlook.com>
2020-03-08 15:48:44 +0200

    fixed small bug in clientapp

--- Client/test.py
+++ Client/test.py
@@ -2,21 +2,23 @@
 # -*- coding: utf-8 -*-
 
 
-import client_s3 as clt
+import client as clt
 import time
+#from struct import *
 
-primes = [8447,6311,941027,909631,49022843,59366651,470202691643,722082735347]
+
+primes = [32, 66, 77, 152, 8447,6311,941027,909631,49022843,59366651,470202691643,722082735347]
 key = []
 
 for i in range(len(primes)):
     key.append(clt.sendRequest(44,primes[i],8))
     time.sleep(1)
-#len(primes)
 for i in range(len(primes)):
-    length, is_prime = clt.getReply(key[i],True)
+    is_prime, length = clt.getReply(key[i],True)
+    #is_prime = unpack('>i', is_prime)
+    is_prime = int.from_bytes(is_prime, byteorder='big', signed=False)
     if is_prime != 0:
         print("\n{} is prime!".format(primes[i]))
     else:
         print("\n{} isn't prime!".format(primes[i]))
     
-        
